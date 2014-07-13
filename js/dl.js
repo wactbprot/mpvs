d3713a57cb40cf29d8c6f5c93c26bef4762089f5
wactbprot <thsteinbock@web.de>
2014-07-13 17:09:00 +0200

    ..

--- js/dl.js
+++ js/dl.js
@@ -5,18 +5,21 @@ $( document ).ready(function() {
   cont.empty();
 
   var addCe = function(){
-
+    // .on('change') synchronisiert
+    // das html (insbesondere die value Attribute)
     $("input").on("change", function(e){
       $(this).attr("value", $(this).val());
     });
 
     $("button").on("click", function(e){
       var parent  = $(this).attr("data-parent"),
-          url     = $(this).attr("data-url");
+          url     = $(this).attr("data-url"),
+          html    = $(this).parent(parent).html();
+
       $.ajax({
         url  : url,
         type : "POST", // the data-method is the method between mpvs and ssmp
-        data : $(this).parent(parent).html(),
+        data : html,
         success: function(data, textStatus, jqXHR){
           replace(true);
         },
