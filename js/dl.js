3ebfd12ec450755972aff638cad5292f66ffe850
wactbprot <thsteinbock@web.de>
2014-07-17 17:47:09 +0200

    s. diff

--- js/dl.js
+++ js/dl.js
@@ -1,50 +1,51 @@
 $( document ).ready(function() {
 
   var cont  = $("#content"),
-      url   = cont.children("a").attr("href"),
-      addCe = function(){
-        // input.on('change')
-        //
-        // synchronisiert
-        // das html (insbesondere die value Attribute)
-        $("input").on("change", function(e){
-          $(this).attr("value", $(this).val());
-        });
-        // button.on('click')
-        //
-        // schickt html unter parent
-        // zurück an mpvs
-        $("button").on("click", function(e){
-          var parent  = $(this).attr("data-parent"),
-              url     = $(this).attr("data-url"),
-              html    = $(this).parent(parent).html();
-          $.ajax({
-            url  : url,
-            type : "POST", // the data-method is the method between mpvs and ssmp
-            data : html,
-            success: function(data, textStatus, jqXHR){
-              replace(true);
-            },
-            error: function (jqXHR, textStatus, errorThrown){
-              console.log(jqXHR);
-              console.log(textStatus);
-              console.log(errorThrown);
-            }
-          });
-        });
-      },
-              replace = function(slow){
-                $.ajax({url:url}).done(function(html){
-                  if(slow){
-                    cont.empty()
-                    .append(html)
-                    .hide()
-                    .fadeIn("fast", addCe);
-                  }else{
-                    cont.empty().append(html);
-                  }
-                });
-              };
+      url   = cont.children("a").attr("href");
+
+  var addCe = function(){
+    // input.on('change')
+    //
+    // synchronisiert
+    // das html (insbesondere die value Attribute)
+    $("input").on("change", function(e){
+      $(this).attr("value", $(this).val());
+    });
+    // button.on('click')
+    //
+    // schickt html unter parent
+    // zurück an mpvs
+    $("button").on("click", function(e){
+      var parent  = $(this).attr("data-parent"),
+          url     = $(this).attr("data-url"),
+          html    = $(this).parent(parent).html();
+      $.ajax({
+        url  : url,
+        type : "POST", // the data-method is the method between mpvs and ssmp
+        data : html,
+        success: function(data, textStatus, jqXHR){
+          //replace(true);
+        },
+        error: function (jqXHR, textStatus, errorThrown){
+          console.log(jqXHR);
+          console.log(textStatus);
+          console.log(errorThrown);
+        }
+      });
+    });
+  };
+  var replace = function(slow){
+    $.ajax({url:url}).done(function(html){
+      if(slow){
+        cont.empty()
+        .append(html)
+        .hide()
+        .fadeIn("fast", addCe);
+      }else{
+        cont.empty().append(html);
+      }
+    });
+  };
   //
   cont.empty();
 
