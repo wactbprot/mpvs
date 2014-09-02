9803c5238e48ea3b535ba62ab4f4a993b74a519e
wactbprot <thsteinbock@web.de>
2014-09-02 12:56:25 +0200

    complete io rewrite; now solved over ssmp collection
    	modified:   css/main.css
    	modified:   js/dl.js
    	modified:   lib/output.js
    	modified:   lib/template.js
    	new file:   templates/display/inputPressureSection.html
    	new file:   templates/display/outputPressureSection.html
    	modified:   templates/display/section.html
    	modified:   templates/element/Type.html
    	modified:   templates/main.html
    	modified:   templates/state.html

--- js/dl.js
+++ js/dl.js
@@ -1,64 +1,58 @@
-$( document ).ready(function() {
 
-  var cont  = $("#content"),
-      url   = cont.children("a").attr("href"),
-      poll  = { state: 300};
 
-  var addCe = function(){
-    // input.on('change')
-    //
-    // synchronisiert
-    // das html (insbesondere die value Attribute)
-    $("input").on("change", function(e){
-      $(this).attr("value", $(this).val());
-    });
-    // button.on('click')
-    //
-    // schickt html unter parent
-    // zurück an mpvs
-    $("button").on("click", function(e){
-      var parent  = $(this).attr("data-parent"),
-          url     = $(this).attr("data-url"),
-          html    = $(this).parent(parent).html();
-      $.ajax({
-        url  : url,
-        type : "POST", // the data-method is the method between mpvs and ssmp
-        data : html,
-        success: function(data, textStatus, jqXHR){
-          //replace(true);
-        },
-        error: function (jqXHR, textStatus, errorThrown){
-          console.log(jqXHR);
-          console.log(textStatus);
-          console.log(errorThrown);
-        }
-      });
-    });
-  };
-  var replace = function(slow){
-    $.ajax({url:url}).done(function(html){
-      if(slow){
-        cont.empty()
-        .append(html)
-        .hide()
-        .fadeIn("fast", addCe);
-      }else{
-        cont.empty().append(html);
+var  replaceHtml = function($elem, cb){
+  var url = $elem.attr("data-url");
+  if(url){
+    $.ajax({url:url})
+    .done(function(html){
+      $elem.empty().append(html);
+    })
+    .done(function(){
+      if($.isFunction(cb)){
+        cb();
       }
     });
-  };
-  //
-  cont.empty();
-
-  if(url){
-    if(cont.hasClass("state")){
-      replace(true);
-      setInterval( replace, poll.state);
-    }
-
-    if(cont.hasClass("io")){
-      replace(true);
-      addCe();
-    }
   }
-});
+};
+
+var  poll = function(){
+  $(".poll").each(function(i){
+    replaceHtml($(this));
+  });
+}
+
+// synchronisiert
+// das html (insbesondere die value Attribute)
+var syncInput = function(){
+  $("input").on("change", function(e){
+    $(this).attr("value", $(this).val());
+  });
+};
+
+// schickt html unter parent
+// zurück an mpvs
+var  armButtons= function(){
+  $("button").on("click", function(e){
+    var parent  = $(this).attr("data-parent"),
+        url     = $(this).attr("data-url"),
+        html    = $(this).parent(parent).html();
+    $.ajax({
+      url  : url,
+      type : "POST", // the data-method is the method between mpvs and ssmp
+      data : html,
+      success: function(data, textStatus, jqXHR){
+      },
+      error: function (jqXHR, textStatus, errorThrown){
+        console.log(textStatus);
+      }
+    });
+  });
+};
+
+$( document ).ready(function() {
+  replaceHtml($("#content").children("a"), function(){
+    armButtons();
+    syncInput()
+  });
+  setInterval( poll, 300);
+})
